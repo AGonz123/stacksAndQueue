56420286d5cc04f3c3c5ee6476768ced1a6efad9
AGonz123 <32293916+AGonz123@users.noreply.github.com>
2017-12-05 16:26:52 -0800

    Update FixedArrayStack.java

--- src/fixedarraystack/FixedArrayStack.java
+++ src/fixedarraystack/FixedArrayStack.java
@@ -1,64 +1,111 @@
-/*
- * To change this license header, choose License Headers in Project Properties.
- * To change this template file, choose Tools | Templates
- * and open the template in the editor.
- */
-package fixedarraystack;
-import java.util.Arrays;
 
 /**
+ * The Class Stack.
  *
- * @author Andrew Gonzales
+ * @param <E> the element type
  */
-public class FixedArrayStack {
-    private T[] stack;
-    private int topIndex;
-    private boolean initialized = false;
-    private static final int DEFAULT_CAPACITY = 50;
-    private static final int MAX_CAPACITY = 10000;
-     public interface StackInterface<T>
-{
-   /** Adds a new entry to the top of this stack.
-       @param newEntry  An object to be added to the stack. */
-   public void push(T newEntry){
-       topIndex++;
-		
-  	if (topIndex >= stack.length) 
-    		
-	stack[topIndex] = newEntry;
-   }
-  
-   public T pop(){
-    T top = null;
-		
-	if (!isEmpty())
-	    {
-		top = stack[topIndex];
-		stack[topIndex] = null;
-		topIndex--; 
-	    }      
-   }
+public class Stack<E > implements IStack<E > {
+	
+	/** The Constant INITIAL_CAPACITY. */
+	private static final int INITIAL_CAPACITY = 10;
 
-   public T peek(){
-     T top = null;
-		
-	if (!isEmpty())
-	    top = stack[topIndex];
-		
-	return top;  
-       }
-  
-   /** Detects whether this stack is empty.
-       @return  True if the stack is empty. */
-   public boolean isEmpty(){
-    return topIndex < 0;
-  
-   }/** Removes all entries from this stack. */
-   public void clear(){
-       for (; topIndex > -1; topIndex--)
-	    stack[topIndex] = null;
-   }
-}
-    }
-    
+	/** The size. */
+	private int size;
+	
+	/** The items. */
+	private E [] items;
+
+	/**
+	 * Instantiates a new stack.
+	 */
+	@SuppressWarnings("unchecked")
+	public Stack () {
+		this.items = (E []) new Object[INITIAL_CAPACITY];
+		size = 0;
+	}
+
+	/**
+	 * Instantiates a new stack.
+	 *
+	 * @param size the size
+	 */
+	@SuppressWarnings("unchecked")
+	public Stack (int size) {
+		this.items = (E []) new Object[size];
+		size = 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see IStack#push(java.lang.Object)
+	 */
+	@Override
+	public void push (E entry) {
+		updateCapacity ();
+		items[size] = entry;
+		size ++;
+	}
+
+	/* (non-Javadoc)
+	 * @see IStack#pop()
+	 */
+	@Override
+	public E pop() {
+		if (!isEmpty()) {
+			size--;
+			return items[size];
+		} else {
+			return null;
+		}
+	}
+
+	/* (non-Javadoc)
+	 * @see IStack#peek()
+	 */
+	@Override
+	public E peek() {
+		return items[size-1];
+	}
+
+	/* (non-Javadoc)
+	 * @see IStack#isEmpty()
+	 */
+	@Override
+	public boolean isEmpty(){
+		return size == 0;
+	}
+
+	/* (non-Javadoc)
+	 * @see IStack#getSize()
+	 */
+	@Override
+	public int getSize () {
+		return size;
+	}
+
+	/* (non-Javadoc)
+	 * @see IStack#clear()
+	 */
+	@Override
+	@SuppressWarnings("unchecked")
+	public void clear() {
+		items = (E []) new Object[INITIAL_CAPACITY];
+		size = 0;						 
+	}
+	
+	/**
+	 * Update capacity.
+	 */
+	@SuppressWarnings("unchecked")
+	private void updateCapacity () {
+		if (size == items.length ) {
+			E [] tempBag = (E [])new Object[size * 2];
+			for (int a = 0; a < size; a++) {
+				tempBag[a] = items[a];
+			}
+			items = tempBag;
+		}
+	}
+
+} // class
+
 
